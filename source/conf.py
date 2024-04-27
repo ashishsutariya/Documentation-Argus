e445b58687ee26a55c0e0bd88e382960fbba5fb8
ashishsutariya <ashishmv1711@gmail.com>
2024-04-27 01:12:46 -0400

    logo update 4

--- source/conf.py
+++ source/conf.py
@@ -19,21 +19,7 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme_options = {
-   
-    'logo_only': True,
-    # 'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    # 'style_external_links': False,
-    # 'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    # 'collapse_navigation': True,
-    # 'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+logo_only=True
 
 
 language = 'English'
